76a11d354ebf458691052c063b2a0e803325baca
Ahmed Salah Eldin <asalaheldin@wisys.com.sa>
2025-08-25 18:09:49 +0200

    Run Parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   retries: 5,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
